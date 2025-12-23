55ae878a0be7fd09bf854716ef7633b40e67b7c7
Tom <ychen306@mit.edu>
2025-12-22 20:22:52 -0500

    CudaKernel

--- cudagen/types.py
+++ cudagen/types.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple
+from abc import ABC
+import ptx
 
 
 @dataclass(frozen=True)
@@ -11,16 +13,19 @@ class Var:
     is_float: bool
     represents_predicate: bool = False
 
+class KernelItem(ABC):
+    pass
+
 
 @dataclass
-class InlineAsm:
+class InlineAsm(KernelItem):
     template: str
     arguments: list[Var]
     outputs: list[Var]
     clobbers_memory: bool = False
 
 @dataclass
-class Load:
+class Load(KernelItem):
     '''
     Models: dest = src[offset]
     assuming everything is byte-typed
@@ -32,15 +37,21 @@ class Load:
     offset: int
 
 @dataclass
-class CudaBranch:
+class CudaBranch(KernelItem):
     cond : Optional[Var]
     target : CudaLabel
 
 @dataclass
-class CudaLabel:
+class CudaLabel(KernelItem):
     name : str
 
+@dataclass
+@dataclass
+class CudaKernel:
+    arguments: list[Tuple[Var, ptx.MemoryDecl]]
+    # this include kernel arguments (i.e., params)
+    var_decls: list[Var]
+    body: list[KernelItem]
 
 # Re-export MemoryDecl for convenience
-import ptx
 MemoryDecl = ptx.MemoryDecl
