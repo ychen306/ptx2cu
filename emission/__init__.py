fb4dc04c38254fec3d039526bd5a9747e11f699c
Tom <ychen306@mit.edu>
2025-12-23 12:50:02 -0500

    declare memory

--- emission/__init__.py
+++ emission/__init__.py
@@ -2,6 +2,7 @@ from .inst import emit_inline_asm_string
 from .param import emit_load, get_type_decl_for_param
 from .kernel import declare_kernel
 from .branch import emit_branch_string
+from .memory import declare_memory
 
 __all__ = [
     "emit_branch_string",
@@ -9,4 +10,5 @@ __all__ = [
     "emit_load",
     "get_type_decl_for_param",
     "declare_kernel",
+    "declare_memory",
 ]
