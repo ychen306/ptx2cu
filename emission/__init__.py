6f75f94b2e1cec736f7e19d6141469e85a434315
Tom <ychen306@mit.edu>
2025-12-23 12:55:09 -0500

    declare local

--- emission/__init__.py
+++ emission/__init__.py
@@ -3,6 +3,7 @@ from .param import emit_load, get_type_decl_for_param
 from .kernel import declare_kernel
 from .branch import emit_branch_string
 from .memory import declare_memory
+from .local import declare_local
 
 __all__ = [
     "emit_branch_string",
@@ -11,4 +12,5 @@ __all__ = [
     "get_type_decl_for_param",
     "declare_kernel",
     "declare_memory",
+    "declare_local",
 ]
